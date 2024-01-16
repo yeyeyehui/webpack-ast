a43a2fe60f401abf943fd90b12f841801657d2f3
60438 <hui.ye@going-link.com>
2024-01-16 15:35:45 +0800

    3

--- ask.js
+++ ask.js
@@ -1,6 +1,6 @@
-http://www.zhufengpeixun.com/front/html/1.9.webpack-loader.html
+http://www.yehuipeixun.com/front/html/1.9.webpack-loader.html
 
-http://www.zhufengpeixun.com/strong/html/103.9.webpack-optimize2.html
+http://www.yehuipeixun.com/strong/html/103.9.webpack-optimize2.html
 
 // "dev": "rollup --config -w" 
 // -w:自动监控文件变化，然后更新
